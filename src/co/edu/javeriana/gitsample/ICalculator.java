ff466552481070db848644fe159d51119eef40b6
Andrés Parra <canesito@gmail.com>
2019-08-14 23:51:14 -0500

    added pow

--- src/co/edu/javeriana/gitsample/ICalculator.java
+++ src/co/edu/javeriana/gitsample/ICalculator.java
@@ -5,5 +5,6 @@ public interface ICalculator {
 	public double substract(double a, double b);
 	public double multiply(double a, double b);
 	public double divide(double a, double b);
+	public double power(double base, double exp);
 	public String about();
 }
