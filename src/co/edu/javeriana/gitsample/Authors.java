21d72a5d3371a68e7b0b3ffd84c838b4e203c798
Andres Camilo Muñoz <ancamu99@hotmail.com>
2019-08-20 02:53:45 -0500

    Actualizacion a master

--- src/co/edu/javeriana/gitsample/Authors.java
+++ src/co/edu/javeriana/gitsample/Authors.java
@@ -9,6 +9,8 @@ public class Authors {
 	public static final String SV="Juan Sebastian Valderrama";
 	public static final String DAMC = "Diego Alejandro Mateus Cruz";
 	public static final String KPN = "Kevin Stid Peláez Nimisica";
+	public static final String ACMU="Andres Camilo Muñoz Usamá";
 	public static final String OA="Other author...";
 	public static final String SAA="Still another author...";
+	
 }
--- src/co/edu/javeriana/gitsample/Main.java
+++ src/co/edu/javeriana/gitsample/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// Create calculator implementation
 		System.out.println("Git Calculator!");
-		 ICalculator calculator = new CalculadoraSV();
+		 ICalculator calculator = new CalculadoraACMU();
 		 System.out.println(calculator.divide(100,0));
 		// calculator.multiply(2,5);
 		// calculator.add(7,5);
